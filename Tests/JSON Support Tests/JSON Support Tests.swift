beed5d9219d71f3e8abe3dd88556c3468e9ebee3
George <GeorgeLyon@users.noreply.github.com>
2025-05-18 10:40:23 -0700

    Make test support harness

--- Tests/JSON Support Tests/JSON Support Tests.swift
+++ Tests/JSON Support Tests/JSON Support Tests.swift
@@ -7,33 +7,77 @@ import Testing
 private struct JSONSupportTests {
 
   @Test
-  func testString() async throws {
-    var decoder = JSON<
-      AsyncStream<[UInt8]>
-    >.StreamingDecoder()
-
-    let (stream, continuation) = AsyncStream<[UInt8]>.makeStream()
-    decoder.reset(fragments: stream)
-    continuation.yield(
-      Array(
-        """
-        "Hello, World!"
-        """.utf8
+  func simpleTest() async throws {
+    try await StringDecoder.decode { decoder in
+      decoder.yield(
+      """
+      "Hello, World!"
+      """
       )
-    )
+      try await #expect(decoder.decodeFragment() == "Hello, World!")
+    }
+  }
+  
+  @Test
+  func escapeCharacters() async throws {
+    try await StringDecoder.decode { decoder in
+      decoder.yield(
+        """
+        "Hello, World\n"
+        """
+      )
+      try await #expect(decoder.decodeFragment() == "Hello, World\n")
+    }
     
-    let (readBytes, fragments) = try await decoder.bytesRead { decoder in
-      try await decoder.readString { decoder in
-        var fragments: [String] = []
-        while let next = try await decoder.decodeNextFragment() {
-          print("Read: \(next)")
-          fragments.append(next)
+    /// Add more tests here
+      
+  }
+  
+}
+
+// MARK: - Implementation Details
+
+private struct StringDecoder: ~Copyable {
+  
+  static func decode(
+    _ body: (inout StringDecoder) async throws -> Void
+  ) async throws {
+    var decoder = StringDecoder()
+    try await body(&decoder)
+  }
+  
+  private init() {
+    var decoder = JSON<AsyncStream<[UInt8]>>.StreamingDecoder()
+    
+    let fragments = AsyncStream<[UInt8]>.makeStream()
+    self.fragmentsContinuation = fragments.continuation
+    decoder.reset(fragments: fragments.stream)
+    
+    let decodedFragments = AsyncThrowingStream<String, Error>.makeStream()
+    self.decodedFragments = decodedFragments.stream.makeAsyncIterator()
+    Task {
+      do {
+        try await decoder.readString { decoder in
+          while let next = try await decoder.decodeNextFragment() {
+            decodedFragments.continuation.yield(next)
+          }
         }
-        return fragments
+        decodedFragments.continuation.finish()
+      } catch {
+        decodedFragments.continuation.finish(throwing: error)
       }
     }
-
-    #expect(String(String.UnicodeScalarView(readBytes.map(UnicodeScalar.init))) == "\"Hello, World!\"")
-    #expect(fragments.joined() == "Hello, World!")
   }
+  
+  mutating func decodeFragment() async throws -> String? {
+    try await decodedFragments.next()
+  }
+  
+  func yield(_ fragment: String) {
+    fragmentsContinuation.yield(Array(fragment.utf8))
+  }
+  
+  private let fragmentsContinuation: AsyncStream<[UInt8]>.Continuation
+  private var decodedFragments: AsyncThrowingStream<String, Error>.AsyncIterator
+  
 }
