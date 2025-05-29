c735c68b2f1d899e54293c7d7651533272167c30
George <GeorgeLyon@users.noreply.github.com>
2025-05-29 11:27:52 -0700

    Array decoding

--- Sources/JSON Support/Decoding/Array Decoding.swift
+++ Sources/JSON Support/Decoding/Array Decoding.swift
@@ -1,3 +1,68 @@
+extension JSON {
+
+  public enum ArrayDecodingResult {
+    case needsMoreData
+
+    /// The read cursor has been advanced to the start of the next element in the array
+    case decodingElement
+
+    /// The array is complete
+    case complete
+  }
+
+}
+
 extension JSON.DecodingStream {
+
+  public mutating func decodeArrayStart() throws -> JSON.ArrayDecodingResult {
+    readWhitespace()
+
+    let start = createCheckpoint()
+
+    switch try read("[").decodingResult() {
+    case .needsMoreData:
+      return .needsMoreData
+    case .decoded:
+      readWhitespace()
+      let isEmpty = try peekCharacter { character in
+        switch character {
+        case "]":
+          return true
+        default:
+          return false
+        }
+      }.decodingResult()
+      switch isEmpty {
+      case .decoded(let isEmpty):
+        return isEmpty ? .complete : .decodingElement
+      case .needsMoreData:
+        /// Restore to start so we don't need to keep track of the fact that we've read "["
+        restore(start)
+        return .needsMoreData
+      }
+    }
+  }
+
+  public mutating func decodeNextArrayElement() throws -> JSON.ArrayDecodingResult {
+    readWhitespace()
+
+    let isComplete = try readCharacter { character in
+      switch character {
+      case "]":
+        return true
+      case ",":
+        return false
+      default:
+        return nil
+      }
+    }.decodingResult()
+
+    switch isComplete {
+    case .needsMoreData:
+      return .needsMoreData
+    case .decoded(let isComplete):
+      return isComplete ? .complete : .decodingElement
+    }
+  }
 
 }
