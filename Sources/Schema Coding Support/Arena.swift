844712a811fa4291029ecae0703581a34a31f703
George <GeorgeLyon@users.noreply.github.com>
2025-11-16 11:22:32 -0800

    New Arena implementation

--- Sources/Schema Coding Support/Arena.swift
+++ Sources/Schema Coding Support/Arena.swift
@@ -1,59 +1,32 @@
-private import BasicContainers
+public final class Arena {
 
-// MARK: - Arena
+  init(_ archetype: Archetype) {
+    archetypeID = archetype.id
 
-public struct Arena: ~Copyable {
+    /// Once an arena has been created from an archetype, we can no longer mutate the archetype
+    archetype.isMutable = false
 
-  public init(
-    minimumBlockSize: Int = 4096
-  ) {
-    self.init(
-      minimumBlockSize: minimumBlockSize,
-      emptyBlocks: .init(),
-      emptyBitwiseCopyableBlocks: .init()
-    )
-  }
-  private init(
-    minimumBlockSize: Int,
-    emptyBlocks: consuming UniqueArray<Block>,
-    emptyBitwiseCopyableBlocks: consuming UniqueArray<BitwiseCopyableBlock>
-  ) {
-    self.minimumBlockSize = minimumBlockSize
-    self.emptyBlocks = emptyBlocks
-    self.emptyBitwiseCopyableBlocks = emptyBitwiseCopyableBlocks
-  }
-
-  public mutating func reset() {
-    var emptyBlocks = self.emptyBlocks
-    while var block = blocks.popLast() {
-      block.reset()
-      emptyBlocks.append(block)
+    slabs = archetype.slabs
+    buffers = slabs.map { slab in
+      slab.allocateAndInitializeBuffer()
     }
-
-    var emptyBitwiseCopyableBlocks = self.emptyBitwiseCopyableBlocks
-    while var block = bitwiseCopyableBlocks.popLast() {
-      block.reset()
-      emptyBitwiseCopyableBlocks.append(block)
-    }
-
-    self = Arena(
-      minimumBlockSize: minimumBlockSize,
-      emptyBlocks: emptyBlocks,
-      emptyBitwiseCopyableBlocks: emptyBitwiseCopyableBlocks
-    )
-
   }
 
-  typealias ID = UniqueID<Arena>
+  func reset() {
+    for (buffer, slab) in zip(buffers, slabs) {
+      slab.reset(buffer)
+    }
+  }
 
-  private let id = ID()
-  private let minimumBlockSize: Int
+  deinit {
+    for (buffer, slab) in zip(buffers, slabs) {
+      slab.deinitializeAndDeallocate(buffer)
+    }
+  }
 
-  private var blocks: UniqueArray<Block> = .init()
-  private var emptyBlocks: UniqueArray<Block>
-
-  private var bitwiseCopyableBlocks: UniqueArray<BitwiseCopyableBlock> = .init()
-  private var emptyBitwiseCopyableBlocks: UniqueArray<BitwiseCopyableBlock>
+  private let archetypeID: Archetype.ID
+  private let buffers: [UnsafeMutableRawBufferPointer]
+  private let slabs: [Slab]
 
 }
 
@@ -61,310 +34,68 @@ public struct Arena: ~Copyable {
 
 extension Arena {
 
-  public subscript<Value>(reference: Reference<Value>) -> Value {
-    get { withValue(for: reference) { $0 } }
-    set { withValue(for: reference) { $0 = newValue } }
-  }
-
-  public func withValue<Value: ~Copyable, T, Failure>(
-    for reference: Reference<Value>,
-    _ operation: (inout Value) throws(Failure) -> T
-  ) throws(Failure) -> T {
-    guard reference.arenaID == id else {
-      fatalError()
-    }
-    return try operation(&reference.pointer.pointee)
-  }
-
   public struct Reference<Value: ~Copyable> {
-    fileprivate let arenaID: Arena.ID
-    fileprivate let pointer: UnsafeMutablePointer<Value>
-  }
-
-}
-
-// MARK: - Data Requiring Deinitialization
-
-extension Arena {
-
-  public mutating func allocate<Value>(_ value: Value) -> Reference<Value> {
-    if let index = blocks.indices.last,
-      blocks[index].canAllocate(Value.self)
-    {
-      return blocks[index].allocate(value, arenaID: id)
-    } else {
-      /// Push this value to an empty block
-      let index = emptyBlocks
-        .indices
-        .reversed()
-        .first { index in
-          emptyBlocks[index].canAllocate(Value.self)
-        }
-      var block: Block
-      if let index {
-        block = emptyBlocks.remove(at: index)
-      } else {
-        /// Create a new block
-        block = Block(
-          minimumByteCount: minimumBlockSize,
-          minimumAlignment: MemoryLayout<Int>.alignment,
-          nextValue: Value.self
-        )
-      }
-
-      let reference = block.allocate(value, arenaID: id)
-      blocks.append(block)
-      return reference
-    }
-  }
-
-  struct Block: ~Copyable {
-
-    public func canAllocate<Value: ~Copyable>(
-      _ value: Value.Type
-    ) -> Bool {
-      buffer.validCursorRange.contains(
-        buffer.cursor
-          .alignedUp(for: Value.self)
-          .advanced(by: MemoryLayout<Value>.size)
-          .alignedUp(for: ValueMetadata.self)
-          .advanced(by: MemoryLayout<ValueMetadata>.size)
-      )
-    }
-
-    public mutating func allocate<Value: ~Copyable>(
-      _ value: consuming Value,
-      arenaID: Arena.ID
-    ) -> Reference<Value> {
-      guard canAllocate(Value.self) else {
-        fatalError()
-      }
-      let valuePointer =
-        buffer.cursor
-        .alignedUp(for: Value.self)
-        .bindMemory(to: Value.self, capacity: 1)
-      valuePointer.initialize(to: value)
-      let metadataPointer =
-        UnsafeMutableRawPointer(valuePointer)
-        .advanced(by: MemoryLayout<Value>.size)
-        .alignedUp(for: ValueMetadata.self)
-        .bindMemory(to: ValueMetadata.self, capacity: 1)
-      metadataPointer.initialize(to: ValueMetadata(type: Value.self))
-      buffer.cursor = UnsafeMutableRawPointer(metadataPointer)
-        .advanced(by: MemoryLayout<ValueMetadata>.size)
-      return Reference(arenaID: arenaID, pointer: valuePointer)
-    }
-
-    /// This must only be called when resetting an `Arena` ensuring the old `Arena.ID` is no longer availble to allow access to data in this block.
-    public mutating func reset() {
-      deinitializeContents()
-      buffer.cursor = buffer.storage.baseAddress!
-    }
-
-    init<Value>(
-      minimumByteCount: Int,
-      minimumAlignment: Int,
-      nextValue: Value.Type
-    ) {
-      buffer = .init(
-        byteCount: max(
-          minimumByteCount,
-          MemoryLayout<Value>.stride(to: ValueMetadata.self) + MemoryLayout<ValueMetadata>.size,
-        ),
-        alignment: max(
-          minimumAlignment,
-          MemoryLayout<Value>.alignment,
-          MemoryLayout<ValueMetadata>.alignment,
-        )
-      )
-    }
-    deinit {
-      /// This should only happen when an `Arena` is deinitialized, which means that no other `Arena` should exist with an ID that would allow references to access data in this block.
-      deinitializeContents()
-    }
-
-    /// All references to data in this block have been invalidated when this is called.
-    ///
-    /// This deinitializes the contents in LIFO order, similar to how exiting a scope works.
-    private func deinitializeContents() {
-      var deallocationCursor = buffer.cursor
-      func pop<Value>(_ value: Value) {
-        deallocationCursor =
-          deallocationCursor
-          /// We assume advancing by a negative size and aligning down is the reverse operation of aligning up and advancing by size.
-          .advanced(by: -MemoryLayout<Value>.size)
-          .alignedDown(for: Value.self)
-          .assumingMemoryBound(to: Value.self)
-          .deinitialize(count: 1)
-          .advanced(by: MemoryLayout<Value>.size)
-      }
-      while deallocationCursor > buffer.storage.baseAddress! {
-        /// The cursor should always be pointing to the end of a metadata segment.
-        /// First, we rewind it to point to the start of the metadata segment.
-        deallocationCursor =
-          deallocationCursor
-          .advanced(by: -MemoryLayout<ValueMetadata>.size)
-        /// Then, we get the type from the metadata
-        let type =
-          deallocationCursor
-          .assumingMemoryBound(to: ValueMetadata.self)
-          .pointee.type
-        /// Finally, we pop a value of that type
-        pop(type)
-      }
-      guard deallocationCursor == buffer.storage.baseAddress! else {
-        fatalError()
-      }
-    }
-
-    /// `Block` stores each value followed by an aligned `ValueMetadata`.
-    /// The cursor always points to either the end of `ValueMetadata` or to the start of the buffer.
-    private var buffer: CursedBuffer
-
-    private struct ValueMetadata {
-      let type: any (~Copyable).Type
-    }
-
-  }
-
-}
-
-// MARK: - Bitwise Copyable Data
-
-extension Arena {
-
-  public mutating func allocate<Value: BitwiseCopyable>(_ value: Value) -> Reference<Value> {
-    if let index = bitwiseCopyableBlocks.indices.last,
-      bitwiseCopyableBlocks[index].canAllocate(Value.self)
-    {
-      return bitwiseCopyableBlocks[index].allocate(value, arenaID: id)
-    } else {
-      /// Push this value to an empty block
-      let index = emptyBitwiseCopyableBlocks
-        .indices
-        .reversed()
-        .first { index in
-          emptyBitwiseCopyableBlocks[index].canAllocate(Value.self)
-        }
-      var block: BitwiseCopyableBlock
-      if let index {
-        block = emptyBitwiseCopyableBlocks.remove(at: index)
-      } else {
-        /// Create a new block
-        block = BitwiseCopyableBlock(
-          minimumByteCount: minimumBlockSize,
-          minimumAlignment: MemoryLayout<Int>.alignment,
-          nextValue: Value.self
-        )
-      }
-
-      let reference = block.allocate(value, arenaID: id)
-      bitwiseCopyableBlocks.append(block)
-      return reference
-    }
-  }
-
-  struct BitwiseCopyableBlock: ~Copyable {
-
-    public func canAllocate<Value: BitwiseCopyable>(
-      _ value: Value.Type
-    ) -> Bool {
-      buffer.validCursorRange.contains(
-        buffer.cursor
-          .alignedUp(for: Value.self)
-          .advanced(by: MemoryLayout<Value>.size)
-      )
-    }
-
-    public mutating func allocate<Value: BitwiseCopyable>(
-      _ value: consuming Value,
-      arenaID: Arena.ID
-    ) -> Reference<Value> {
-      let pointer = buffer.cursor
-        .alignedUp(for: Value.self)
-        .bindMemory(to: Value.self, capacity: 1)
-      pointer.initialize(to: value)
-      buffer.cursor = UnsafeMutableRawPointer(pointer)
-        .advanced(by: MemoryLayout<Value>.size)
-      return Reference(arenaID: arenaID, pointer: pointer)
-    }
-
-    /// This must only be called when resetting an `Arena` ensuring the old `Arena.ID` is no longer availble to allow access to data in this block.
-    public mutating func reset() {
-      buffer.cursor = buffer.storage.baseAddress!
-    }
-
-    init<Value>(
-      minimumByteCount: Int,
-      minimumAlignment: Int,
-      nextValue: Value.Type
-    ) {
-      buffer = .init(
-        byteCount: max(minimumByteCount, MemoryLayout<Value>.size),
-        alignment: max(minimumAlignment, MemoryLayout<Value>.alignment)
-      )
-    }
-
-    private var buffer: CursedBuffer
-
-  }
-
-}
-
-// MARK: - Support
-
-extension Arena {
-
-  private struct CursedBuffer: ~Copyable {
-
     init(
-      byteCount: Int,
-      alignment: Int
+      archetypeID: Arena.Archetype.ID,
+      buffer: BufferReference,
+      offset: Int
     ) {
-      storage = .allocate(byteCount: byteCount, alignment: alignment)
-      cursor = storage.baseAddress!
+      self.archetypeID = archetypeID
+      self.buffer = buffer
+      self.offset = offset
     }
-    deinit {
-      /// This should only happen when an `Arena` is deinitialized, which means that no other `Arena` should exist with an ID that would allow references to access data in this block.
-      storage.deallocate()
+    fileprivate let archetypeID: Arena.Archetype.ID
+    fileprivate let buffer: BufferReference
+    fileprivate let offset: Int
+  }
+
+  struct BufferReference {
+    fileprivate let index: Int
+  }
+
+}
+
+// MARK: - Archetype
+
+extension Arena {
+
+  public final class Archetype {
+
+    public init() {
+      heterogenousSlab = .init(archetypeID: id, buffer: .init(index: 0))
+      bitwiseCopyableSlap = .init(archetypeID: id, buffer: .init(index: 1))
+      slabs = [heterogenousSlab, bitwiseCopyableSlap]
     }
 
-    let storage: UnsafeMutableRawBufferPointer
-
-    var cursor: UnsafeMutableRawPointer {
-      didSet {
-        guard cursor == storage.baseAddress! || oldValue < cursor else {
-          /// The only valid operations are moving the cursor forward or resetting it to the start
-          fatalError()
-        }
-        guard validCursorRange.contains(cursor) else {
-          fatalError()
-        }
-      }
+    public func typedSlab<Component: ~Copyable>(
+      _ type: Component?.Type
+    ) -> TypedSlab<Component?> {
+      let slab = TypedSlab<Component?>(
+        archetypeID: id,
+        buffer: .init(index: slabs.count)
+      )
+      slabs.append(slab)
+      return slab
     }
 
-    var validCursorRange: ClosedRange<UnsafeMutableRawPointer> {
-      let baseAddress = storage.baseAddress!
-      return baseAddress...baseAddress.advanced(by: storage.count)
-    }
-
-    private init(storage: UnsafeMutableRawBufferPointer) {
-      self.storage = storage
-      self.cursor = storage.baseAddress!
-    }
+    fileprivate let id: Arena.Archetype.ID = .makeUnique()
+    fileprivate let heterogenousSlab: HeterogenousSlab
+    fileprivate let bitwiseCopyableSlap: HeterogenousSlab
+    fileprivate var slabs: [Slab] = []
+    fileprivate var isMutable: Bool = true
 
   }
 
 }
 
-extension MemoryLayout {
+// MARK: - Slab
 
-  /// - Returns:
-  ///     The stride from a value of T to an aligned value of U.
-  ///     T must be aligned to the greater of the alignment of T and U.
-  fileprivate static func stride<U>(to type: U.Type) -> Int {
-    let alignment = MemoryLayout<U>.alignment
-    return (size + alignment - 1) & ~(alignment - 1)
+extension Arena {
+
+  protocol Slab {
+    func allocateAndInitializeBuffer() -> UnsafeMutableRawBufferPointer
+    func reset(_ buffer: UnsafeMutableRawBufferPointer)
+    func deinitializeAndDeallocate(_ buffer: UnsafeMutableRawBufferPointer)
   }
 
 }
---
+++ Sources/Schema Coding Support/Bitwise Copyable Slab.swift
@@ -0,0 +1,139 @@
+extension Arena {
+
+  final class BitwiseCopyableSlab: Arena.Slab {
+
+    init(
+      archetypeID: Arena.Archetype.ID,
+      buffer: Arena.BufferReference
+    ) {
+      self.archetypeID = archetypeID
+      self.buffer = buffer
+    }
+    private let archetypeID: Arena.Archetype.ID
+    private let buffer: Arena.BufferReference
+    private var components: [Component] = []
+    private var alignment: Int = MemoryLayout<Int>.alignment
+    private var byteCount: Int = 0
+    private var isMutable: Bool = false
+
+    func append<Value: BitwiseCopyable>(
+      _ type: Value?.Type
+    ) -> Arena.Reference<Value?> {
+      guard isMutable == true else { fatalError() }
+      components.append(Component(typeExistential: type))
+
+      let componentAlignment = MemoryLayout<Value?>.alignment
+      alignment = max(alignment, componentAlignment)
+      let offset = (byteCount + componentAlignment - 1) & ~(componentAlignment - 1)
+      byteCount = offset + MemoryLayout<Value?>.size
+
+      return Arena.Reference(
+        archetypeID: archetypeID,
+        buffer: buffer,
+        offset: offset
+      )
+    }
+
+    func allocateAndInitializeBuffer() -> UnsafeMutableRawBufferPointer {
+      isMutable = false
+      let buffer =
+        UnsafeMutableRawBufferPointer
+        .allocate(
+          byteCount: byteCount,
+          alignment: alignment
+        )
+      forEachComponent(in: buffer) { component, pointer in
+        component.initialize(pointer)
+      }
+      return buffer
+    }
+
+    func reset(_ buffer: UnsafeMutableRawBufferPointer) {
+      guard isMutable == false else { fatalError() }
+      forEachComponent(in: buffer) { component, pointer in
+        component.reset(pointer)
+      }
+    }
+
+    func deinitializeAndDeallocate(_ buffer: UnsafeMutableRawBufferPointer) {
+      guard isMutable == false else { fatalError() }
+      forEachComponent(in: buffer) { component, pointer in
+        component.deinitialize(pointer)
+      }
+      buffer.deallocate()
+    }
+
+  }
+
+}
+
+// MARK: - Components
+
+extension Arena.BitwiseCopyableSlab {
+
+  private func forEachComponent(
+    in buffer: UnsafeMutableRawBufferPointer,
+    _ body: (Component, UnsafeMutableRawPointer) -> Void
+  ) {
+    var cursor = buffer.baseAddress!
+    let endPointer: UnsafeMutableRawPointer = buffer.baseAddress! + buffer.count
+    for component in components {
+      let pointer = component.increment(&cursor)
+      assert(cursor <= endPointer)
+      body(component, pointer)
+    }
+    assert(cursor == endPointer)
+  }
+
+  fileprivate protocol ComponentType {
+    static func initialValue() -> Self
+  }
+
+  fileprivate struct Component {
+
+    let typeExistential: any (ComponentType & BitwiseCopyable).Type
+
+    func increment(_ cursor: inout UnsafeMutableRawPointer) -> UnsafeMutableRawPointer {
+      func increment<T: ComponentType & BitwiseCopyable>(_ type: T.Type) -> UnsafeMutableRawPointer
+      {
+        let pointer = cursor.alignedUp(for: type)
+        cursor = pointer + MemoryLayout<T>.size
+        return pointer
+      }
+      return increment(typeExistential)
+    }
+
+    func initialize(_ pointer: UnsafeMutableRawPointer) {
+      func initialize<T: ComponentType & BitwiseCopyable>(_ type: T.Type) {
+        pointer
+          .bindMemory(to: T.self, capacity: 1)
+          .initialize(to: T.initialValue())
+      }
+      initialize(typeExistential)
+    }
+
+    func reset(_ pointer: UnsafeMutableRawPointer) {
+      func reset<T: ComponentType & BitwiseCopyable>(_ type: T.Type) {
+        pointer
+          .assumingMemoryBound(to: T.self)
+          .pointee = T.initialValue()
+      }
+      reset(typeExistential)
+    }
+
+    func deinitialize(_ pointer: UnsafeMutableRawPointer) {
+      func deinitialize<T: ComponentType & BitwiseCopyable>(_ type: T.Type) {
+        pointer
+          .assumingMemoryBound(to: T.self)
+          .deinitialize(count: 1)
+      }
+      deinitialize(typeExistential)
+    }
+
+  }
+
+}
+
+extension Optional: Arena.BitwiseCopyableSlab.ComponentType {
+  fileprivate static func initialValue() -> Wrapped? { nil }
+}
