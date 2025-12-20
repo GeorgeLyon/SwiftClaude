18f0bdaad662a9d26ab1a35085417c9ff1d309f7
George <GeorgeLyon@users.noreply.github.com>
2025-12-19 19:18:27 -0800

    Complete. Batshit. Insanity.

--- Tests/Schema Coding Tests/Schema Coding Tests.swift
+++ Tests/Schema Coding Tests/Schema Coding Tests.swift
@@ -11,90 +11,59 @@ struct SchemaCodingTests {
     try test("hello", isCodedAs: "\"hello\"")
   }
 
-  @Test
-  func objectWithStringPropertyCoding() throws {
-    let schema = SchemaCoding.Support.objectSchema(
-      propertyName: CodingKeys.self
-    ) {
-      SchemaCoding.Support.objectProperty(
-        name: CodingKeys.name,
-        schema: SchemaCoding.Support.schema(representing: String.self)
-      )
-    }
-    try schema.test(("Alice"), isCodedAs: "{\"name\":\"Alice\"}")
-  }
+  // MARK: - Object Schema
 
-  @Test
-  func metaMetaMetaSchemaEncoding() throws {
-    let schema = SchemaCoding.Support.objectSchema(
-      propertyName: CodingKeys.self
-    ) {
-      SchemaCoding.Support.objectProperty(
-        name: CodingKeys.name,
-        schema: SchemaCoding.Support.schema(representing: String.self)
-      )
-    }
-    let context = SchemaCoding.Support.SchemaContext()
-    let metaSchema = schema.metaSchema(in: context)
-    let metaMetaSchema = metaSchema.metaSchema(in: context)
-    let metaMetaMetaSchema = metaMetaSchema.metaSchema(in: context)
+  @Suite("Object Schema")
+  struct ObjectSchema {
 
-    try metaSchema.test(
-      schema,
-      encodesAs: """
-        {
-          "properties": {
-            "name": {
-              "type": "string"
-            }
-          }
-        }
-        """,
-      prettyPrint: true
-    )
-    try metaMetaSchema.test(
-      metaSchema,
-      encodesAs: """
-        {
-          "properties": {
-            "description": {
-              "type": "string"
-            },
+    @Test
+    func objectWithStringPropertyCoding() throws {
+      let schema = SchemaCoding.Support.objectSchema(
+        propertyName: CodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: CodingKeys.name,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      try schema.test(("Alice"), isCodedAs: "{\"name\":\"Alice\"}")
+    }
+
+    @Test
+    func metaMetaMetaSchemaEncoding() throws {
+      let schema = SchemaCoding.Support.objectSchema(
+        propertyName: CodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: CodingKeys.name,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let context = SchemaCoding.Support.SchemaContext()
+      let metaSchema = schema.metaSchema(in: context)
+      let metaMetaSchema = metaSchema.metaSchema(in: context)
+      let metaMetaMetaSchema = metaMetaSchema.metaSchema(in: context)
+
+      try metaSchema.test(
+        schema,
+        encodesAs: """
+          {
             "properties": {
               "name": {
-                "properties": {
-                  "description": {
-                    "type": "string"
-                  },
-                  "type": {
-                    "const": "string"
-                  }
-                }
+                "type": "string"
               }
             }
           }
-        }
-        """,
-      prettyPrint: true
-    )
-    try metaMetaMetaSchema.test(
-      metaMetaSchema,
-      encodesAs: """
-        {
-          "properties": {
-            "description": {
-              "type": "string"
-            },
+          """,
+        prettyPrint: true
+      )
+      try metaMetaSchema.test(
+        metaSchema,
+        encodesAs: """
+          {
             "properties": {
               "description": {
-                "properties": {
-                  "description": {
-                    "type": "string"
-                  },
-                  "type": {
-                    "const": "string"
-                  }
-                }
+                "type": "string"
               },
               "properties": {
                 "name": {
@@ -102,24 +71,61 @@ struct SchemaCodingTests {
                     "description": {
                       "type": "string"
                     },
+                    "type": {
+                      "const": "string"
+                    }
+                  }
+                }
+              }
+            }
+          }
+          """,
+        prettyPrint: true
+      )
+      try metaMetaMetaSchema.test(
+        metaMetaSchema,
+        encodesAs: """
+          {
+            "properties": {
+              "description": {
+                "type": "string"
+              },
+              "properties": {
+                "description": {
+                  "properties": {
+                    "description": {
+                      "type": "string"
+                    },
+                    "type": {
+                      "const": "string"
+                    }
+                  }
+                },
+                "properties": {
+                  "name": {
                     "properties": {
                       "description": {
-                        "properties": {
-                          "description": {
-                            "type": "string"
-                          },
-                          "type": {
-                            "const": "string"
-                          }
-                        }
+                        "type": "string"
                       },
-                      "type": {
-                        "properties": {
-                          "description": {
-                            "type": "string"
-                          },
-                          "const": {
-                            "type": "string"
+                      "properties": {
+                        "description": {
+                          "properties": {
+                            "description": {
+                              "type": "string"
+                            },
+                            "type": {
+                              "const": "string"
+                            }
+                          }
+                        },
+                        "type": {
+                          "properties": {
+                            "description": {
+                              "type": "string"
+                            },
+                            "const": {
+                              "type": "string"
+                            }
                           }
                         }
                       }
@@ -129,14 +135,303 @@ struct SchemaCodingTests {
               }
             }
           }
-        }
-        """,
-      prettyPrint: true
-    )
+          """,
+        prettyPrint: true
+      )
+    }
+
+  }
+
+  // MARK: - Composite Object Schema
+
+  @Suite("Composite Object Schema")
+  struct CompositeObjectSchema {
+
+    @Test
+    func compositeObjectWithTwoStringPropertiesCoding() throws {
+      let schema1 = SchemaCoding.Support.objectSchema(
+        propertyName: CodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: CodingKeys.name,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let schema2 = SchemaCoding.Support.objectSchema(
+        propertyName: AgeCodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: AgeCodingKeys.age,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let compositeSchema = SchemaCoding.Support.CompositeObjectSchema(schema1, schema2)
+      try compositeSchema.test(
+        (("Alice"), ("25")),
+        isCodedAs: "{\"name\":\"Alice\",\"age\":\"25\"}"
+      )
+    }
+
+    @Test
+    func compositeObjectEncodingPropertyOrder() throws {
+      let schema1 = SchemaCoding.Support.objectSchema(
+        propertyName: CodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: CodingKeys.name,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let schema2 = SchemaCoding.Support.objectSchema(
+        propertyName: AgeCodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: AgeCodingKeys.age,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let compositeSchema = SchemaCoding.Support.CompositeObjectSchema(schema1, schema2)
+      try compositeSchema.test(
+        (("Bob"), ("30")),
+        encodesAs: "{\"name\":\"Bob\",\"age\":\"30\"}"
+      )
+    }
+
+    @Test
+    func compositeObjectDecodingDifferentPropertyOrder() throws {
+      let schema1 = SchemaCoding.Support.objectSchema(
+        propertyName: CodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: CodingKeys.name,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let schema2 = SchemaCoding.Support.objectSchema(
+        propertyName: AgeCodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: AgeCodingKeys.age,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let compositeSchema = SchemaCoding.Support.CompositeObjectSchema(schema1, schema2)
+      try compositeSchema.test(
+        "{\"age\":\"42\",\"name\":\"Charlie\"}",
+        decodesAs: (("Charlie"), ("42"))
+      )
+    }
+
+    @Test
+    func compositeObjectMetaSchemaEncoding() throws {
+      let schema1 = SchemaCoding.Support.objectSchema(
+        propertyName: CodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: CodingKeys.name,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let schema2 = SchemaCoding.Support.objectSchema(
+        propertyName: AgeCodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: AgeCodingKeys.age,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let compositeSchema = SchemaCoding.Support.CompositeObjectSchema(schema1, schema2)
+      let context = SchemaCoding.Support.SchemaContext()
+      let metaSchema = compositeSchema.metaSchema(in: context)
+
+      try metaSchema.test(
+        compositeSchema,
+        encodesAs: """
+          {
+            "properties": {
+              "name": {
+                "type": "string"
+              },
+              "age": {
+                "type": "string"
+              }
+            }
+          }
+          """,
+        prettyPrint: true
+      )
+    }
+
+    @Test
+    func compositeObjectMetaMetaMetaSchemaEncoding() throws {
+      let schema1 = SchemaCoding.Support.objectSchema(
+        propertyName: CodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: CodingKeys.name,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let schema2 = SchemaCoding.Support.objectSchema(
+        propertyName: AgeCodingKeys.self
+      ) {
+        SchemaCoding.Support.objectProperty(
+          name: AgeCodingKeys.age,
+          schema: SchemaCoding.Support.schema(representing: String.self)
+        )
+      }
+      let compositeSchema = SchemaCoding.Support.CompositeObjectSchema(schema1, schema2)
+      let context = SchemaCoding.Support.SchemaContext()
+      let metaSchema = compositeSchema.metaSchema(in: context)
+      let metaMetaSchema = metaSchema.metaSchema(in: context)
+      let metaMetaMetaSchema = metaMetaSchema.metaSchema(in: context)
+
+      try metaSchema.test(
+        compositeSchema,
+        encodesAs: """
+          {
+            "properties": {
+              "name": {
+                "type": "string"
+              },
+              "age": {
+                "type": "string"
+              }
+            }
+          }
+          """,
+        prettyPrint: true
+      )
+      try metaMetaSchema.test(
+        metaSchema,
+        encodesAs: """
+          {
+            "properties": {
+              "description": {
+                "type": "string"
+              },
+              "properties": {
+                "name": {
+                  "properties": {
+                    "description": {
+                      "type": "string"
+                    },
+                    "type": {
+                      "const": "string"
+                    }
+                  }
+                },
+                "age": {
+                  "properties": {
+                    "description": {
+                      "type": "string"
+                    },
+                    "type": {
+                      "const": "string"
+                    }
+                  }
+                }
+              }
+            }
+          }
+          """,
+        prettyPrint: true
+      )
+      try metaMetaMetaSchema.test(
+        metaMetaSchema,
+        encodesAs: """
+          {
+            "properties": {
+              "description": {
+                "type": "string"
+              },
+              "properties": {
+                "description": {
+                  "properties": {
+                    "description": {
+                      "type": "string"
+                    },
+                    "type": {
+                      "const": "string"
+                    }
+                  }
+                },
+                "properties": {
+                  "name": {
+                    "properties": {
+                      "description": {
+                        "type": "string"
+                      },
+                      "properties": {
+                        "description": {
+                          "properties": {
+                            "description": {
+                              "type": "string"
+                            },
+                            "type": {
+                              "const": "string"
+                            }
+                          }
+                        },
+                        "type": {
+                          "properties": {
+                            "description": {
+                              "type": "string"
+                            },
+                            "const": {
+                              "type": "string"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  },
+                  "age": {
+                    "properties": {
+                      "description": {
+                        "type": "string"
+                      },
+                      "properties": {
+                        "description": {
+                          "properties": {
+                            "description": {
+                              "type": "string"
+                            },
+                            "type": {
+                              "const": "string"
+                            }
+                          }
+                        },
+                        "type": {
+                          "properties": {
+                            "description": {
+                              "type": "string"
+                            },
+                            "const": {
+                              "type": "string"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          """,
+        prettyPrint: true
+      )
+    }
+
   }
 
 }
 
 private enum CodingKeys: String, CodingKey {
   case name
+}
+
+private enum AgeCodingKeys: String, CodingKey {
+  case age
 }
